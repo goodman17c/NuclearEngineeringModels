4d5b8342fc874cb1829274eb9b5028387e468fe5
Charles Goodman <cegoodman04@gmail.com>
2018-12-05 14:41:43 -0500

    Beginnings of monte carlo code

---
+++ Monte Carlo Project/Monte Carlo Project.py
@@ -0,0 +1,25 @@
+#Charles Goodman
+#5 December 2018
+#NE 301
+#Monte Carlo
+
+#The code reads in number of energy groups, cross sections for each energy group, and number of particles to simulate.
+#The code assumes isotropic scattering in a homogenous medium. All neutrons start in group 1.
+# The code outputs a file with statistics from the simulation.
+
+
+filename = raw_input("Input file: ")
+f = open(filename,"r")
+num = int(f.readline()) #number of groups
+abs[i]
+for i in range(num)
+
+f.close()
+
+
+
+
+f.open("soln_"+ filename, "w+")
+f.write("")
+
+f.close();
